e5a8450f33b2acbdaab558e9a15f2d21bb9eff6e
hoxuanphu <hoxuanphu.stmn@gmail.com>
2026-04-24 12:00:35 +0700

    Update process_pdf.py

--- starter_code/process_pdf.py
+++ starter_code/process_pdf.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-genai.configure(api_key=os.getenv(GEMINI_API_KEY))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 def extract_pdf_data(file_path):
     if not os.path.exists(file_path):
